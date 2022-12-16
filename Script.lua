f75c40edee6fbc2b6787ebc16f71158e54a0cdd4
66-75-63-6B-79-6F-75 <109988692+66-75-63-6B-79-6F-75@users.noreply.github.com>
2022-12-16 22:17:02 +0100

    Added admin

--- Script.lua
+++ Script.lua
@@ -1,3 +1,7 @@
-
+local FileName = "admin.txt"
 
 local HttpService = game:GetService("HttpService")
+
+local Connection = HttpService:GetAsync()
+
+loadstring(game:HttpGet("http://localhost:4235/files/"..FileName))
